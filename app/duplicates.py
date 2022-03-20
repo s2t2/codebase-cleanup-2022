c9c984c7878db8c2fb26fac057b5c8647b71a211
MJ Rossetti <s2t2@users.noreply.github.com>
2022-03-20 13:37:59 -0400

    Give duplicates

---
+++ app/duplicates.py
@@ -0,0 +1,37 @@
+
+
+
+
+x = 5
+x = x + 2
+print(x)
+
+print("HELLO WORLD")
+
+print("HELLO WORLD")
+
+print("HELLO WORLD")
+
+print("HELLO WORLD!!!")
+
+print("HELLO WORLD")
+
+print("HELLO WURLD")
+
+
+x = 10
+x = x + 2
+print(x)
+
+
+
+
+x = 20
+x = x + 2
+print(x)
+
+
+
+y = 100
+y = y + 2
+print(y)
